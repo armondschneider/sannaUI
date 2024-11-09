cf934ec2230451fcfdc4527dabaa2b22a2ff0987
Armond Schneider <mondoloves@icloud.com>
2024-11-08 20:18:05 -0700

    added first button components

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './styles/global.css';
+import './styles/tailwind.css';
+
+// Import components
+import './components/button/button.js';
+
+// Initialized
+console.log('sannaUI library loaded successfully');
